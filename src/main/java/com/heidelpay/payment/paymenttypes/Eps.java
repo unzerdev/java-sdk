c23199777d8d344f37844255739df31aedc7d389
rene.felder <rene.felder@192.168.45.200>
2019-01-08 17:22:56 +0100

    AHC-634 - Add EPS to Java SDK

--- src/main/java/com/heidelpay/payment/paymenttypes/Eps.java
+++ src/main/java/com/heidelpay/payment/paymenttypes/Eps.java
@@ -1,5 +1,14 @@
 package com.heidelpay.payment.paymenttypes;
 
+import java.math.BigDecimal;
+import java.net.URL;
+import java.util.Currency;
+
+import com.heidelpay.payment.Charge;
+import com.heidelpay.payment.Customer;
+import com.heidelpay.payment.Heidelpay;
+import com.heidelpay.payment.communication.HttpCommunicationException;
+
 /*-
  * #%L
  * Heidelpay Java SDK
@@ -22,9 +31,38 @@ package com.heidelpay.payment.paymenttypes;
 
 public class Eps extends AbstractPaymentType implements PaymentType {
 
+	private String bic;
+	
+	public Eps(String bic) {
+		this.bic = bic;
+	}
+
+	public Eps() {
+		super();
+	}
+
+	public Eps(Heidelpay heidelpay) {
+		super(heidelpay);
+	}
+
 	@Override
 	public String getTypeUrl() {
 		return "types/eps";
+	}
+
+	public String getBic() {
+		return bic;
+	}
+
+	public void setBic(String bic) {
+		this.bic = bic;
+	}
+
+	public Charge charge(BigDecimal amount, Currency currency, URL returnUrl) throws HttpCommunicationException {
+		return getHeidelpay().charge(amount, currency, this, returnUrl, (Customer)null);
+	}
+	public Charge charge(BigDecimal amount, Currency currency, URL returnUrl, Customer customer) throws HttpCommunicationException {
+		return getHeidelpay().charge(amount, currency, this, returnUrl, customer);
 	}
 
 }
