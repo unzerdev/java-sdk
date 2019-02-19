1c81c616886ad1ce2a6e9231d491ffec45a018b0
dirk.dorsch <dirk.dorsch@heidelpay.com>
2019-02-19 17:38:28 +0100

    AHC-853 throw Exception in case of error-json returned from api

---
+++ src/test/java/com/heidelpay/payment/TestData.java
@@ -0,0 +1,23 @@
+package com.heidelpay.payment;
+
+public class TestData {
+
+	public static String errorJson() {
+		return "{" + 
+				"  \"id\" : \"s-err-f2ea241e5e8e4eb3b1513fab12c\"," + 
+				"  \"url\" : \"https://api.heidelpay.com/v1/payments/charges\"," + 
+				"  \"timestamp\" : \"2019-01-09 15:42:24\"," + 
+				"  \"errors\" : [ {\r\n" + 
+				"    \"code\" : \"COR.400.100.101\"," + 
+				"    \"merchantMessage\" : \"Address untraceable\",\r\n" + 
+				"    \"customerMessage\" : \"The provided address is invalid. Please check your input and try agian.\"," + 
+				"    \"status\" : {" + 
+				"      \"successful\" : false," + 
+				"      \"processing\" : false," + 
+				"      \"pending\" : false" + 
+				"    }" + 
+				"  } ]" + 
+				"}";
+	}
+	
+}
