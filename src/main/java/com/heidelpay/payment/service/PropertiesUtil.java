a60dec6019f9a3d71b669644a075962f17811e41
Kai Himken <kai.himken@heidelpay.com>
2020-11-02 10:53:23 +0100

    SDK-251 The JavaSDK cannot be used * Added Support for CommandLine-Arguments instead of POM-Arguments for MGW-Keys which are needed by the Test-Functions * The Format is: -Dpublickey1=[Insert PublicKey] -Dprivatekey1=[Insert PrivateKey1] -Dprivatekey2=[Insert PrivateKey2] -Dprivatekey3=[Insert PrivateKey3] -DmarketplacePrivatekey=[Insert MarketPlacePrivateKey]

--- src/main/java/com/heidelpay/payment/service/PropertiesUtil.java
+++ src/main/java/com/heidelpay/payment/service/PropertiesUtil.java
@@ -9,9 +9,9 @@ package com.heidelpay.payment.service;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -20,13 +20,12 @@ package com.heidelpay.payment.service;
  * #L%
  */
 
-import java.io.IOException;
-import java.util.Properties;
-
+import com.heidelpay.payment.exceptions.PropertiesException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.heidelpay.payment.exceptions.PropertiesException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class PropertiesUtil {
 	public static final Logger logger = LogManager.getLogger(PropertiesUtil.class);
@@ -40,21 +39,25 @@ public class PropertiesUtil {
 	public static final String MARKETPLACE_PRIVATE_KEY = "marketplacePrivatekey";
 
 	private Properties properties;
-	
+
 	private void loadProperties() {
 		try {
 			Properties loadedProperties = new Properties();
+
 			loadedProperties.load(this.getClass().getResourceAsStream("/heidelpay.properties"));
 			this.properties = loadedProperties;
 
-			loadedProperties.load(this.getClass().getResourceAsStream("/test-keys.properties"));
-			this.properties.putAll(loadedProperties);
+			this.properties.put(PUBLIC_KEY1, System.getProperty(PUBLIC_KEY1));
+			this.properties.put(PRIVATE_KEY1, System.getProperty(PRIVATE_KEY1));
+			this.properties.put(PRIVATE_KEY2, System.getProperty(PRIVATE_KEY2));
+			this.properties.put(PRIVATE_KEY3, System.getProperty(PRIVATE_KEY3));
+			this.properties.put(MARKETPLACE_PRIVATE_KEY, System.getProperty(MARKETPLACE_PRIVATE_KEY));
 		} catch (IOException e) {
 			logger.error("Error loading heidelpay.properties from Classpath: %s", e.getMessage());
 			throw new PropertiesException("Error loading heidelpay.properties from Classpath: " + e.getMessage());
 		}
 	}
-	
+
 	public String getString(String key) {
 		return getProperties().getProperty(key);
 	}
