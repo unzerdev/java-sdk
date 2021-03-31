574bc4c7579052c9ee4f56e7ce1ec8794d329cdb
Kai Himken <kai.himken@heidelpay.com>
2021-03-31 15:14:42 +0200

    UMCS-159: Provide ApplePay Merchant Validation - Added ApplePaySession-Class - Added ApplePayAdapter-Class with functionality to Validate a Merchant against the ApplePay-System - Added ApplePayAdapterTest-Class

---
+++ src/test/java/com/unzer/payment/util/ApplePayAdapterTest.java
@@ -0,0 +1,15 @@
+package com.unzer.payment.util;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+public class ApplePayAdapterTest {
+
+    @Test(expected = NullPointerException.class)
+    public void IfAllParametrsAreNullThrowError() throws NoSuchAlgorithmException, IOException, KeyManagementException {
+        ApplePayAdapter.validateApplePayMerchant(null, null, null, null);
+    }
+}
