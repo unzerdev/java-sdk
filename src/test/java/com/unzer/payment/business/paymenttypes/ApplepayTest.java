b7d4cc53e5a9b6a4cef2b13f3812fd8d34abf27c
Kai Himken <kai.himken@heidelpay.com>
2021-04-08 17:41:47 +0200

    UMCS-161: Unit- and Integration-Tests - Added new Tests and changed existing Tests

--- src/test/java/com/unzer/payment/business/paymenttypes/ApplepayTest.java
+++ src/test/java/com/unzer/payment/business/paymenttypes/ApplepayTest.java
@@ -22,11 +22,11 @@ package com.unzer.payment.business.paymenttypes;
 
 import com.unzer.payment.Authorization;
 import com.unzer.payment.Charge;
+import com.unzer.payment.PaymentException;
 import com.unzer.payment.business.AbstractPaymentTest;
 import com.unzer.payment.communication.HttpCommunicationException;
 import com.unzer.payment.paymenttypes.Applepay;
 import com.unzer.payment.paymenttypes.ApplepayHeader;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.math.BigDecimal;
@@ -39,7 +39,6 @@ import static org.junit.Assert.assertNotNull;
 
 public class ApplepayTest extends AbstractPaymentTest {
 
-	@Ignore("missing key configuration for apple pay")
 	@Test
 	public void testCreateApplepayType() throws HttpCommunicationException {
 		Applepay applepay = getApplePay();
@@ -48,12 +47,11 @@ public class ApplepayTest extends AbstractPaymentTest {
 		assertNotNull(response.getId());
 		assertNotNull(response.getExpiryDate());
 		assertNotNull(response.getNumber());
-		assertEquals("481852******2027", response.getNumber());
-		assertEquals("12/2023", response.getExpiryDate());
+		assertEquals("520424******6937", response.getNumber());
+		assertEquals("09/2022", response.getExpiryDate());
 		
 	}
 
-	@Ignore("missing key configuration for apple pay")
 	@Test
 	public void testAuthorizeApplePayType() throws HttpCommunicationException, MalformedURLException {
 		Applepay applepay = getUnzer().createPaymentType(getApplePay());
@@ -63,7 +61,6 @@ public class ApplepayTest extends AbstractPaymentTest {
 		assertEquals(Authorization.Status.SUCCESS, authorization.getStatus());
 	}
 
-	@Ignore("missing key configuration for apple pay")
 	@Test
 	public void testAuthorizeApplePayTypeId() throws HttpCommunicationException, MalformedURLException {
 		Applepay applepay = getUnzer().createPaymentType(getApplePay());
@@ -73,7 +70,26 @@ public class ApplepayTest extends AbstractPaymentTest {
 		assertEquals(Authorization.Status.SUCCESS, authorization.getStatus());
 	}
 
-	@Ignore("missing key configuration for apple pay")
+	@Test
+	public void testAuthorizeApplePayTypeIdWithBigAmount() throws HttpCommunicationException, MalformedURLException {
+		Applepay applepay = getUnzer().createPaymentType(getApplePay());
+		Authorization authorization = getUnzer().authorize(BigDecimal.valueOf(10000L), Currency.getInstance("EUR"), applepay.getId(), new URL("https://www.meinShop.de"));
+		assertNotNull(authorization);
+		assertNotNull(authorization.getId());
+		assertEquals(Authorization.Status.SUCCESS, authorization.getStatus());
+	}
+
+	@Test
+	public void testAuthorizeApplePayTypeIdWithZeroAmount() throws HttpCommunicationException, MalformedURLException {
+		Applepay applepay = getUnzer().createPaymentType(getApplePay());
+
+		try{
+			getUnzer().authorize(BigDecimal.ZERO, Currency.getInstance("EUR"), applepay.getId(), new URL("https://www.meinShop.de"));
+		}catch (PaymentException paymentException){
+			assertEquals(Integer.valueOf(400), paymentException.getStatusCode());
+		}
+	}
+
 	@Test
 	public void testChargeApplePayType() throws HttpCommunicationException, MalformedURLException {
 		Applepay applepay = getUnzer().createPaymentType(getApplePay());
@@ -83,7 +99,25 @@ public class ApplepayTest extends AbstractPaymentTest {
 		assertEquals(Authorization.Status.SUCCESS, charge.getStatus());
 	}
 
-	@Ignore("missing key configuration for apple pay")
+	@Test
+	public void testChargeApplePayTypeWithBigAmount() throws HttpCommunicationException, MalformedURLException {
+		Applepay applepay = getUnzer().createPaymentType(getApplePay());
+		Charge charge = applepay.charge(BigDecimal.valueOf(10000L), Currency.getInstance("EUR"), new URL("https://www.meinShop.de"));
+		assertNotNull(charge);
+		assertNotNull(charge.getId());
+		assertEquals(Authorization.Status.SUCCESS, charge.getStatus());
+	}
+
+	@Test
+	public void testChargeApplePayTypeWithZeroAmount() throws HttpCommunicationException, MalformedURLException {
+		Applepay applepay = getUnzer().createPaymentType(getApplePay());
+		try{
+			applepay.charge(BigDecimal.ZERO, Currency.getInstance("EUR"), new URL("https://www.meinShop.de"));
+		}catch (PaymentException paymentException){
+			assertEquals(Integer.valueOf(400), paymentException.getStatusCode());
+		}
+	}
+
 	@Test
 	public void testChargeApplePayTypeId() throws HttpCommunicationException, MalformedURLException {
 		Applepay applepay = getUnzer().createPaymentType(getApplePay());
@@ -93,16 +127,34 @@ public class ApplepayTest extends AbstractPaymentTest {
 		assertEquals(Authorization.Status.SUCCESS, charge.getStatus());
 	}
 
+	@Test
+	public void testChargeApplePayTypeIdWithBigAmount() throws HttpCommunicationException, MalformedURLException {
+		Applepay applepay = getUnzer().createPaymentType(getApplePay());
+		Charge charge = getUnzer().charge(BigDecimal.valueOf(10000L), Currency.getInstance("EUR"), applepay.getId(), new URL("https://www.meinShop.de"));
+		assertNotNull(charge);
+		assertNotNull(charge.getId());
+		assertEquals(Authorization.Status.SUCCESS, charge.getStatus());
+	}
+
+	@Test
+	public void testChargeApplePayTypeIdWithZeroAmount() throws HttpCommunicationException, MalformedURLException {
+		Applepay applepay = getUnzer().createPaymentType(getApplePay());
+		try{
+			getUnzer().charge(BigDecimal.ZERO, Currency.getInstance("EUR"), applepay.getId(), new URL("https://www.meinShop.de"));
+		}catch (PaymentException paymentException){
+			assertEquals(Integer.valueOf(400), paymentException.getStatusCode());
+		}
+	}
 
 	private Applepay getApplePay() {
 		Applepay applepay = new Applepay();
 		applepay.setVersion("EC_v1");
-		applepay.setData("RlUOw14KcaH/85Ln/ROds26cROnVO2dOLBcODQDuwXCkuMogbeRteRoy+TlfZAYKfPyAPopQKPauzDsSEaLaLyns6Nqln28mh8SHilBsBJZUb/pU5xKNfsq2eSmk8obRqkrDiIi8pVsi7UXUIMHF3XkP7DPX84IFTwlelDD8YbDAGsihDYNjTwjg+jX3Q9RJmKpG/DM8lDtE3A2HA8wUruVrOFal/nEITq8c3jK8cnL/GVZMrBDsPiHdGxTuzf0O4XddV0A24GE6M1Y6CWp3nVBUMfyedTHggCg/HGU4sFDlCnoX2A39imwL35tYZ83wUHiPJ36el2nGlNkYR7zQ0SQWxDjS+tS68gD19JBhrNYqdl567b6njMGaP19mXdpkLZXhVr3pWYXDzNZXi6HjmvVUqFR/Z+UwapasulnNbg==");
-		applepay.setSignature("MIAGCSqGSIb3DQEHAqCAMIACAQExDzANBglghkgBZQMEAgEFADCABgkqhkiG9w0BBwEAAKCAMIID5jCCA4ugAwIBAgIIaGD2mdnMpw8wCgYIKoZIzj0EAwIwejEuMCwGA1UEAwwlQXBwbGUgQXBwbGljYXRpb24gSW50ZWdyYXRpb24gQ0EgLSBHMzEmMCQGA1UECwwdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAYTAlVTMB4XDTE2MDYwMzE4MTY0MFoXDTIxMDYwMjE4MTY0MFowYjEoMCYGA1UEAwwfZWNjLXNtcC1icm9rZXItc2lnbl9VQzQtU0FOREJPWDEUMBIGA1UECwwLaU9TIFN5c3RlbXMxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAYTAlVTMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEgjD9q8Oc914gLFDZm0US5jfiqQHdbLPgsc1LUmeY+M9OvegaJajCHkwz3c6OKpbC9q+hkwNFxOh6RCbOlRsSlaOCAhEwggINMEUGCCsGAQUFBwEBBDkwNzA1BggrBgEFBQcwAYYpaHR0cDovL29jc3AuYXBwbGUuY29tL29jc3AwNC1hcHBsZWFpY2EzMDIwHQYDVR0OBBYEFAIkMAua7u1GMZekplopnkJxghxFMAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUI/JJxE+T5O8n5sT2KGw/orv9LkswggEdBgNVHSAEggEUMIIBEDCCAQwGCSqGSIb3Y2QFATCB/jCBwwYIKwYBBQUHAgIwgbYMgbNSZWxpYW5jZSBvbiB0aGlzIGNlcnRpZmljYXRlIGJ5IGFueSBwYXJ0eSBhc3N1bWVzIGFjY2VwdGFuY2Ugb2YgdGhlIHRoZW4gYXBwbGljYWJsZSBzdGFuZGFyZCB0ZXJtcyBhbmQgY29uZGl0aW9ucyBvZiB1c2UsIGNlcnRpZmljYXRlIHBvbGljeSBhbmQgY2VydGlmaWNhdGlvbiBwcmFjdGljZSBzdGF0ZW1lbnRzLjA2BggrBgEFBQcCARYqaHR0cDovL3d3dy5hcHBsZS5jb20vY2VydGlmaWNhdGVhdXRob3JpdHkvMDQGA1UdHwQtMCswKaAnoCWGI2h0dHA6Ly9jcmwuYXBwbGUuY29tL2FwcGxlYWljYTMuY3JsMA4GA1UdDwEB/wQEAwIHgDAPBgkqhkiG92NkBh0EAgUAMAoGCCqGSM49BAMCA0kAMEYCIQDaHGOui+X2T44R6GVpN7m2nEcr6T6sMjOhZ5NuSo1egwIhAL1a+/hp88DKJ0sv3eT3FxWcs71xmbLKD/QJ3mWagrJNMIIC7jCCAnWgAwIBAgIISW0vvzqY2pcwCgYIKoZIzj0EAwIwZzEbMBkGA1UEAwwSQXBwbGUgUm9vdCBDQSAtIEczMSYwJAYDVQQLDB1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwHhcNMTQwNTA2MjM0NjMwWhcNMjkwNTA2MjM0NjMwWjB6MS4wLAYDVQQDDCVBcHBsZSBBcHBsaWNhdGlvbiBJbnRlZ3JhdGlvbiBDQSAtIEczMSYwJAYDVQQLDB1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATwFxGEGddkhdUaXiWBB3bogKLv3nuuTeCN/EuT4TNW1WZbNa4i0Jd2DSJOe7oI/XYXzojLdrtmcL7I6CmE/1RFo4H3MIH0MEYGCCsGAQUFBwEBBDowODA2BggrBgEFBQcwAYYqaHR0cDovL29jc3AuYXBwbGUuY29tL29jc3AwNC1hcHBsZXJvb3RjYWczMB0GA1UdDgQWBBQj8knET5Pk7yfmxPYobD+iu/0uSzAPBgNVHRMBAf8EBTADAQH/MB8GA1UdIwQYMBaAFLuw3qFYM4iapIqZ3r6966/ayySrMDcGA1UdHwQwMC4wLKAqoCiGJmh0dHA6Ly9jcmwuYXBwbGUuY29tL2FwcGxlcm9vdGNhZzMuY3JsMA4GA1UdDwEB/wQEAwIBBjAQBgoqhkiG92NkBgIOBAIFADAKBggqhkjOPQQDAgNnADBkAjA6z3KDURaZsYb7NcNWymK/9Bft2Q91TaKOvvGcgV5Ct4n4mPebWZ+Y1UENj53pwv4CMDIt1UQhsKMFd2xd8zg7kGf9F3wsIW2WT8ZyaYISb1T4en0bmcubCYkhYQaZDwmSHQAAMYIBjTCCAYkCAQEwgYYwejEuMCwGA1UEAwwlQXBwbGUgQXBwbGljYXRpb24gSW50ZWdyYXRpb24gQ0EgLSBHMzEmMCQGA1UECwwdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAYTAlVTAghoYPaZ2cynDzANBglghkgBZQMEAgEFAKCBlTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0xOTA4MjAxNTU0NDhaMCoGCSqGSIb3DQEJNDEdMBswDQYJYIZIAWUDBAIBBQChCgYIKoZIzj0EAwIwLwYJKoZIhvcNAQkEMSIEIMzc1EKBbOahu8M/V42JIZ2B4XYrvKBINeoyckmFydijMAoGCCqGSM49BAMCBEgwRgIhANJLZ8Szn/3hExHnQEDytXOtdBAForbNO36Z+38XJRedAiEA6KPlEcRrxbK1P1ingetpWDHXpmWZ9WMGFCT0jtgYvNEAAAAAAAA=");
+		applepay.setData("lY5mL0Vf1EGwgz1UNCgf2bUCJEFpMCIpMQK8FrsBepqXNOCNk+JH+hcbQ75yckhyqSO4oe5UCxmcb3RrGMSkqs6CozbY5fwJdLq6Tsz8SkFyhulP+2fHtBzgIfmV6cLp09cfzUL10nPJ8ec+g4+Xklbqz+IM9mXpnss6VYNMolv1SA27ZWjYQfcWOiWlQ6KRiNadjoylZhp3BeJF4Ky2MDHIpxMAsvADADrjQqTsz5dqWhzp4un56xcz1dJhcsAV62Od2q62xRUJp3uPNoL0/P4ld3zD9DpjPfGGatyizx/t5CI7TXH0jN3gfcPgCXuhlO9f0c6cG8GIvSdswUnhebJdbZGbUdPWJT/bEPEATvPwhjLJkgRyM86xd/vyXBXs8AYyRS46lbriits9aivi7AKoGy0Q+IIlMPl3YGEfpyBkvljNFK9O5K4EVP6xsRvdviTndJWywxkGXHhMxMZgDyiDySP7MKHh5UeWpIG9X7cHIsCRkPyteSZlHJ+m+wvfGpXlma0l7IdOeVpfvthLWGdY3sGaRYmeOLNHB8EdOatlkTODk8wC9rI=");
+		applepay.setSignature("MIAGCSqGSIb3DQEHAqCAMIACAQExDzANBglghkgBZQMEAgEFADCABgkqhkiG9w0BBwEAAKCAMIID5jCCA4ugAwIBAgIIaGD2mdnMpw8wCgYIKoZIzj0EAwIwejEuMCwGA1UEAwwlQXBwbGUgQXBwbGljYXRpb24gSW50ZWdyYXRpb24gQ0EgLSBHMzEmMCQGA1UECwwdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAYTAlVTMB4XDTE2MDYwMzE4MTY0MFoXDTIxMDYwMjE4MTY0MFowYjEoMCYGA1UEAwwfZWNjLXNtcC1icm9rZXItc2lnbl9VQzQtU0FOREJPWDEUMBIGA1UECwwLaU9TIFN5c3RlbXMxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAYTAlVTMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEgjD9q8Oc914gLFDZm0US5jfiqQHdbLPgsc1LUmeY+M9OvegaJajCHkwz3c6OKpbC9q+hkwNFxOh6RCbOlRsSlaOCAhEwggINMEUGCCsGAQUFBwEBBDkwNzA1BggrBgEFBQcwAYYpaHR0cDovL29jc3AuYXBwbGUuY29tL29jc3AwNC1hcHBsZWFpY2EzMDIwHQYDVR0OBBYEFAIkMAua7u1GMZekplopnkJxghxFMAwGA1UdEwEB/wQCMAAwHwYDVR0jBBgwFoAUI/JJxE+T5O8n5sT2KGw/orv9LkswggEdBgNVHSAEggEUMIIBEDCCAQwGCSqGSIb3Y2QFATCB/jCBwwYIKwYBBQUHAgIwgbYMgbNSZWxpYW5jZSBvbiB0aGlzIGNlcnRpZmljYXRlIGJ5IGFueSBwYXJ0eSBhc3N1bWVzIGFjY2VwdGFuY2Ugb2YgdGhlIHRoZW4gYXBwbGljYWJsZSBzdGFuZGFyZCB0ZXJtcyBhbmQgY29uZGl0aW9ucyBvZiB1c2UsIGNlcnRpZmljYXRlIHBvbGljeSBhbmQgY2VydGlmaWNhdGlvbiBwcmFjdGljZSBzdGF0ZW1lbnRzLjA2BggrBgEFBQcCARYqaHR0cDovL3d3dy5hcHBsZS5jb20vY2VydGlmaWNhdGVhdXRob3JpdHkvMDQGA1UdHwQtMCswKaAnoCWGI2h0dHA6Ly9jcmwuYXBwbGUuY29tL2FwcGxlYWljYTMuY3JsMA4GA1UdDwEB/wQEAwIHgDAPBgkqhkiG92NkBh0EAgUAMAoGCCqGSM49BAMCA0kAMEYCIQDaHGOui+X2T44R6GVpN7m2nEcr6T6sMjOhZ5NuSo1egwIhAL1a+/hp88DKJ0sv3eT3FxWcs71xmbLKD/QJ3mWagrJNMIIC7jCCAnWgAwIBAgIISW0vvzqY2pcwCgYIKoZIzj0EAwIwZzEbMBkGA1UEAwwSQXBwbGUgUm9vdCBDQSAtIEczMSYwJAYDVQQLDB1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwHhcNMTQwNTA2MjM0NjMwWhcNMjkwNTA2MjM0NjMwWjB6MS4wLAYDVQQDDCVBcHBsZSBBcHBsaWNhdGlvbiBJbnRlZ3JhdGlvbiBDQSAtIEczMSYwJAYDVQQLDB1BcHBsZSBDZXJ0aWZpY2F0aW9uIEF1dGhvcml0eTETMBEGA1UECgwKQXBwbGUgSW5jLjELMAkGA1UEBhMCVVMwWTATBgcqhkjOPQIBBggqhkjOPQMBBwNCAATwFxGEGddkhdUaXiWBB3bogKLv3nuuTeCN/EuT4TNW1WZbNa4i0Jd2DSJOe7oI/XYXzojLdrtmcL7I6CmE/1RFo4H3MIH0MEYGCCsGAQUFBwEBBDowODA2BggrBgEFBQcwAYYqaHR0cDovL29jc3AuYXBwbGUuY29tL29jc3AwNC1hcHBsZXJvb3RjYWczMB0GA1UdDgQWBBQj8knET5Pk7yfmxPYobD+iu/0uSzAPBgNVHRMBAf8EBTADAQH/MB8GA1UdIwQYMBaAFLuw3qFYM4iapIqZ3r6966/ayySrMDcGA1UdHwQwMC4wLKAqoCiGJmh0dHA6Ly9jcmwuYXBwbGUuY29tL2FwcGxlcm9vdGNhZzMuY3JsMA4GA1UdDwEB/wQEAwIBBjAQBgoqhkiG92NkBgIOBAIFADAKBggqhkjOPQQDAgNnADBkAjA6z3KDURaZsYb7NcNWymK/9Bft2Q91TaKOvvGcgV5Ct4n4mPebWZ+Y1UENj53pwv4CMDIt1UQhsKMFd2xd8zg7kGf9F3wsIW2WT8ZyaYISb1T4en0bmcubCYkhYQaZDwmSHQAAMYIBjDCCAYgCAQEwgYYwejEuMCwGA1UEAwwlQXBwbGUgQXBwbGljYXRpb24gSW50ZWdyYXRpb24gQ0EgLSBHMzEmMCQGA1UECwwdQXBwbGUgQ2VydGlmaWNhdGlvbiBBdXRob3JpdHkxEzARBgNVBAoMCkFwcGxlIEluYy4xCzAJBgNVBAYTAlVTAghoYPaZ2cynDzANBglghkgBZQMEAgEFAKCBlTAYBgkqhkiG9w0BCQMxCwYJKoZIhvcNAQcBMBwGCSqGSIb3DQEJBTEPFw0yMTA0MDgxNDM0MTZaMCoGCSqGSIb3DQEJNDEdMBswDQYJYIZIAWUDBAIBBQChCgYIKoZIzj0EAwIwLwYJKoZIhvcNAQkEMSIEINwcxAsrag446wVrj5/rJU5X6QF3LvrR0vbOBrTGEVrEMAoGCCqGSM49BAMCBEcwRQIhAMz2/4qXPh7Tu8Qcf7Edi1zXppSGaZO1LEkQ/1hwfET3AiAHht4JGfrhsLl5CwFg4vuHP1p/ZjbWlt9ei+n6FpRTywAAAAAAAA==");
 		ApplepayHeader header = new ApplepayHeader();
-		header.setEphemeralPublicKey("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEyGsYCP+pw8dG9pgt0s/mrJFGqYD0fYwzyXACXAlpN6ghVZpxDPsdh4jiBQBgd4tvedSFVbw+nH/FsN/bs/hK1Q==");
-		header.setPublicKeyHash("M2yzlpBsH3GwH5jTV9GgKC7bAUdeIOIfjwQhoKjg5+s=");
-		header.setTransactionId("93a2574ba3e42f24b25160cd91fc8d170c9abe9520997767b51f5099933d96f5");
+		header.setEphemeralPublicKey("MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEvObBjuehAktBfudn9rM3uUmK4S2Nq1h3O9FemVRDvjJf8c8ClVo7dHHaBrgKiYzR0vBS0Yk8f+u62cILj5yqEA==");
+		header.setPublicKeyHash("zqO5Y3ldWWm4NnIkfGCvJILw30rp3y46Jsf21gE8CNg=");
+		header.setTransactionId("f4c9127c624f57cf4882911bcdaae3c279bda34bf28d9933bd5615932146ec1e");
 		applepay.setHeader(header);
 		return applepay;
 	}
