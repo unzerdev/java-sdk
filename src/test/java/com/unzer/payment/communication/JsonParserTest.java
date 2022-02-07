0fa1b33fa5920e29961f8f7bded39d98f79483cb
Kai Himken <kai.himken@heidelpay.com>
2022-02-07 17:41:38 +0100

    UMCS-326: Added a Json-Validation before real Json-Deserialization happens and added tests for it. Positive Tests for `fromJson` are not possible because of recursive references inside most of the Payment-relevant Classes (e.g. Unzer reference).

--- src/test/java/com/unzer/payment/communication/JsonParserTest.java
+++ src/test/java/com/unzer/payment/communication/JsonParserTest.java
@@ -9,9 +9,9 @@ package com.unzer.payment.communication;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -21,53 +21,70 @@ package com.unzer.payment.communication;
  */
 
 
-import com.unzer.payment.PaymentError;
-import com.unzer.payment.PaymentException;
-import com.unzer.payment.TestData;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.unzer.payment.*;
+import com.unzer.payment.business.AbstractPaymentTest;
 import com.unzer.payment.communication.json.JsonCharge;
 import com.unzer.payment.communication.json.JsonErrorObject;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import java.net.MalformedURLException;
 
-public class JsonParserTest {
+import static org.junit.Assert.*;
 
-	@Test
-	public void given_an_error_message_then_payment_exception_is_thrown() {
-		try {
-			JsonParser parser = new JsonParser();
-			parser.fromJson(TestData.errorJson(), JsonCharge.class);
+public class JsonParserTest extends AbstractPaymentTest {
 
-			fail("Expected PaymentException");
-		} catch (PaymentException exception) {
-			assertBasicErrorAttributes(exception.getId(), exception.getUrl(), exception.getTimestamp());
-			assertPaymentError(exception.getPaymentErrorList().get(0));
-		}
-	}
+    @Test
+    public void given_an_error_message_then_payment_exception_is_thrown() {
+        try {
+            JsonParser parser = new JsonParser();
+            parser.fromJson(TestData.errorJson(), JsonCharge.class);
 
-	@Test
-	public void given_an_error_json_then_fromJson_returnes_jsonerrorobject() {
-		assertJsonError(new JsonParser().fromJson(TestData.errorJson(), JsonErrorObject.class));
-	}
+            fail("Expected PaymentException");
+        } catch (PaymentException exception) {
+            assertBasicErrorAttributes(exception.getId(), exception.getUrl(), exception.getTimestamp());
+            assertPaymentError(exception.getPaymentErrorList().get(0));
+        }
+    }
 
-	private void assertJsonError(JsonErrorObject expectedError) {
-		assertBasicErrorAttributes(expectedError.getId(), expectedError.getUrl(), expectedError.getTimestamp());
-		assertEquals(1, expectedError.getErrors().size());
-		assertPaymentError(expectedError.getErrors().get(0));
-	}
+    @Test
+    public void given_an_error_json_then_fromJson_returnes_jsonerrorobject() {
+        assertJsonError(new JsonParser().fromJson(TestData.errorJson(), JsonErrorObject.class));
+    }
 
-	private void assertBasicErrorAttributes(String id, String url, String timestamp) {
-		assertEquals("s-err-f2ea241e5e8e4eb3b1513fab12c", id);
-		assertEquals("https://api.unzer.com/v1/payments/charges", url);
-		assertEquals("2019-01-09 15:42:24", timestamp);
+    @Test
+    public void testValidJson() {
+        assertTrue(new JsonParser().isJsonValid("{\"name\": \"value\"}"));
+    }
 
-	}
+    @Test
+    public void testInvalidJson() {
+        assertFalse(new JsonParser().isJsonValid("This is an error message!"));
+    }
 
-	private void assertPaymentError(PaymentError error) {
-		assertEquals("COR.400.100.101", error.getCode());
-		assertEquals("Address untraceable", error.getMerchantMessage());
-		assertEquals("The provided address is invalid. Please check your input and try agian.",
-				error.getCustomerMessage());
-	}
+    @Test(expected = IllegalArgumentException.class)
+    public void testFromInvalidJson() {
+        new JsonParser().fromJson("This is an error message!", Payment.class);
+    }
+
+    private void assertJsonError(JsonErrorObject expectedError) {
+        assertBasicErrorAttributes(expectedError.getId(), expectedError.getUrl(), expectedError.getTimestamp());
+        assertEquals(1, expectedError.getErrors().size());
+        assertPaymentError(expectedError.getErrors().get(0));
+    }
+
+    private void assertBasicErrorAttributes(String id, String url, String timestamp) {
+        assertEquals("s-err-f2ea241e5e8e4eb3b1513fab12c", id);
+        assertEquals("https://api.unzer.com/v1/payments/charges", url);
+        assertEquals("2019-01-09 15:42:24", timestamp);
+
+    }
+
+    private void assertPaymentError(PaymentError error) {
+        assertEquals("COR.400.100.101", error.getCode());
+        assertEquals("Address untraceable", error.getMerchantMessage());
+        assertEquals("The provided address is invalid. Please check your input and try agian.",
+                error.getCustomerMessage());
+    }
 }
