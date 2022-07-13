320f166fa4b71a13816c477021f856cc551bb2b1
Vladislav Lipianin <>
2022-07-13 11:14:12 +0200

    UMC-519(change) Fix deserialization

--- src/main/java/com/unzer/payment/communication/JsonURLConverter.java
+++ src/main/java/com/unzer/payment/communication/JsonURLConverter.java
@@ -9,9 +9,9 @@ package com.unzer.payment.communication;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -35,24 +35,28 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 
 public class JsonURLConverter
-		implements JsonDeserializer<URL>, JsonSerializer<URL> {
-	public static final Logger logger = LogManager.getLogger(JsonURLConverter.class);
+        implements JsonDeserializer<URL>, JsonSerializer<URL> {
+    public static final Logger logger = LogManager.getLogger(JsonURLConverter.class);
 
-	@Override
-	public URL deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
-		String urlValue = json.getAsJsonPrimitive().getAsString();
-		if (urlValue == null || "".equalsIgnoreCase(urlValue)) return null;
-		try {
-			return new URL(urlValue);
-		} catch (MalformedURLException e) {
-			logger.warn("Invalid URL '{}': {}", urlValue, e.getMessage());
-			return null;
-		}
-	}
+    @Override
+    public URL deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) {
+        String urlValue = json.getAsJsonPrimitive().getAsString();
 
-	@Override
-	public JsonElement serialize(URL src, Type typeOfSrc, JsonSerializationContext context) {
-		return new JsonPrimitive(src.toString());
-	}
+        if (urlValue == null || "".equalsIgnoreCase(urlValue)) {
+            return null;
+        }
+
+        try {
+            return new URL(urlValue);
+        } catch (MalformedURLException e) {
+            logger.warn("Invalid URL '{}': {}", urlValue, e.getMessage());
+            return null;
+        }
+    }
+
+    @Override
+    public JsonElement serialize(URL src, Type typeOfSrc, JsonSerializationContext context) {
+        return new JsonPrimitive(src.toString());
+    }
 
 }
