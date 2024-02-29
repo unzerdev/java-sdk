b2b0dc30ea74d16049fa58f2c8fba132945b1d42
Muharrem Xhixha <m.xhixha.ext@unzer.com>
2024-02-29 13:08:07 +0100

    CC-731 : Add charge tests

--- src/test/java/com/unzer/payment/business/paymenttypes/ClickToPayTest.java
+++ src/test/java/com/unzer/payment/business/paymenttypes/ClickToPayTest.java
@@ -1,13 +1,21 @@
 package com.unzer.payment.business.paymenttypes;
 
 import com.github.tomakehurst.wiremock.junit5.WireMockTest;
+import com.unzer.payment.Charge;
 import com.unzer.payment.Unzer;
 import com.unzer.payment.communication.HttpClientMock;
 import com.unzer.payment.communication.JsonParser;
+import com.unzer.payment.models.googlepay.IntermediateSigningKey;
+import com.unzer.payment.models.googlepay.SignedKey;
+import com.unzer.payment.models.googlepay.SignedMessage;
 import com.unzer.payment.paymenttypes.ClickToPay;
+import com.unzer.payment.paymenttypes.GooglePay;
+import org.checkerframework.checker.units.qual.C;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.Collections;
+import java.util.Currency;
 import java.util.Objects;
 import java.util.Scanner;
 
@@ -60,5 +68,45 @@ public class ClickToPayTest {
         assertEquals(false, clickToPay.getRecurring());
         assertEquals("0:0:0:0:0:0:0:1", clickToPay.getGeoLocation().getClientIp());
         assertNull(clickToPay.getGeoLocation().getCountryIsoA2());
+    }
+
+    @Test
+    void test_charge_ok() {
+        Unzer unzer = new Unzer(new HttpClientMock(), "s-private-key");
+
+        stubFor(
+                post("/v1/types/clicktopay/")
+                        .willReturn(jsonResponse(getResponse("type.json"), 200))
+        );
+
+        stubFor(
+                get("/v1/types/clicktopay/s-ctp-q0nucec6itwe")
+                        .willReturn(jsonResponse(getResponse("type.json"), 200))
+        );
+
+        stubFor(
+                post("/v1/payments/charges/")
+                        .willReturn(jsonResponse(getResponse("charge.json"), 200))
+        );
+
+        stubFor(
+                get("/v1/payments/s-pay-300")
+                        .willReturn(jsonResponse(getResponse("payment-charge.json"), 200))
+        );
+        stubFor(
+                get("/v1/payments/s-pay-300/charges/s-chg-2")
+                        .willReturn(jsonResponse(getResponse("charge.json"), 200))
+        );
+
+
+        ClickToPay type = new ClickToPay()
+                .setBrand("mastercard")
+                .setMcMerchantTransactionId("0a4e0d3.34f4a04b.894125b16ddd1f1b3a58273d63a0894179ac3535")
+                .setMcCxFlowId("34f4a04b.5ab95e32-30f7-483f-846f-a08230a6d2ed.1618397078")
+                .setMcCorrelationId("corr12345");
+
+
+
+        Charge charge = unzer.charge(BigDecimal.TEN, Currency.getInstance("EUR"), type);
     }
 }
