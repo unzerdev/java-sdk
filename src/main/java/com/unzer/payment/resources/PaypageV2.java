e298f87a9fafa5c733ae3712d780b0e258fda7e9
david.owusu <david.owusu@unzer.com>
2024-09-10 13:53:42 +0200

    [CC-1315] Add Linkpay: - add linkpay properties

--- src/main/java/com/unzer/payment/resources/PaypageV2.java
+++ src/main/java/com/unzer/payment/resources/PaypageV2.java
@@ -9,6 +9,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.HashMap;
 
 @Data
@@ -41,6 +42,11 @@ public class PaypageV2 extends BaseResource {
     private HashMap<String, PaymentMethodConfig> paymentMethodsConfigs;
     private Risk risk;
 
+    // Linkpay only.
+    protected String alias;
+    protected Boolean multiUse;
+    protected Date expiresAt;
+
     // Response Parameter
     @Setter(AccessLevel.PRIVATE)
     private String redirectUrl;
@@ -48,6 +54,9 @@ public class PaypageV2 extends BaseResource {
     private PaypagePayment[] payments;
     @Setter(AccessLevel.PRIVATE)
     private Integer total;
+
+    @Setter(AccessLevel.PRIVATE)
+    protected String qrCodeSvG;
 
     public PaypageV2(BigDecimal amount, String currency, String mode) {
         this.amount = amount;
