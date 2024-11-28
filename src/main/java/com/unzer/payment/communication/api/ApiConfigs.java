ae8c3a410b98a04c146d576f7a718356743e2985
david.owusu <david.owusu@unzer.com>
2024-11-28 16:53:19 +0100

    set stg env urls.

--- src/main/java/com/unzer/payment/communication/api/ApiConfigs.java
+++ src/main/java/com/unzer/payment/communication/api/ApiConfigs.java
@@ -2,7 +2,7 @@ package com.unzer.payment.communication.api;
 
 public class ApiConfigs {
 
-    public static final ApiConfig PAYMENT_API = new ApiConfig("https://api.unzer.com", "https://dev-api.unzer.com");
-    public static final ApiConfig TOKEN_SERVICE_API = new ApiConfig("https://token.upcgw.com", "https://token.test.upcgw.com");
-    public static final ApiConfig PAYPAGE_API = new ApiConfig("https://paypage.unzer.com", "https://paypage.test.unzer.io", ApiConfig.AuthMethod.BEARER);
+    public static final ApiConfig PAYMENT_API = new ApiConfig("https://api.unzer.com", "https://stg-api.unzer.com");
+    public static final ApiConfig TOKEN_SERVICE_API = new ApiConfig("https://token.upcgw.com", "https://token.int.unzer.io");
+    public static final ApiConfig PAYPAGE_API = new ApiConfig("https://paypage.unzer.com", "https://paypage.int.unzer.io", ApiConfig.AuthMethod.BEARER);
 }
