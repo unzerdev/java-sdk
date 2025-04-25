f0bdf43392e79d6801b49950a8357c6c7a645b83
David Owusu <33735090+Ryouzanpaku@users.noreply.github.com>
2025-04-25 10:45:34 +0200

    [CC-2358] release 5.6.0 (#173)

--- src/main/java/com/unzer/payment/BasketV3.java
+++ src/main/java/com/unzer/payment/BasketV3.java
@@ -1,5 +1,13 @@
 package com.unzer.payment;
 
+/**
+ * This is a prototype of the v3 Basket resource.
+ * <p>
+ * This class represents version 3 of Basket resource in the Unzer API.
+ * The version uses bearer authentication for API calls.
+ * Make sure to use the same Unzer instance to use the same JWT token across multiple calls.
+ * Also, the resource ID incorporates UUID and has a length of 42.
+ */
 public class BasketV3 extends Basket {
     @Override
     protected String getResourceUrl() {
