d5906ae3258f2b6c04eda55504efa979aeedf1aa
david.owusu <david.owusu@unzer.com>
2025-08-20 10:55:24 +0200

    [CC-2673] Add wero method.

--- src/main/java/com/unzer/payment/models/WeroTransactionData.java
+++ src/main/java/com/unzer/payment/models/WeroTransactionData.java
@@ -1,9 +1,7 @@
 package com.unzer.payment.models;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeName;
-import com.google.gson.annotations.SerializedName;
 import lombok.Data;
 
 /**
@@ -14,47 +12,5 @@ import lombok.Data;
 @JsonTypeName("wero")
 @JsonTypeInfo(include = JsonTypeInfo.As.WRAPPER_OBJECT, use = JsonTypeInfo.Id.NAME)
 public class WeroTransactionData {
-
     private EventDependentPayment eventDependentPayment;
-
-    @Data
-    public static class EventDependentPayment {
-        private CaptureTrigger captureTrigger;
-        private AmountPaymentType amountPaymentType;
-        /**
-         * Maximum time from authorization to capture in seconds.
-         */
-        private Integer maxAuthToCaptureTime;
-        /**
-         * Whether multiple captures are allowed.
-         */
-        private Boolean multiCapturesAllowed;
-    }
-
-    public enum CaptureTrigger {
-        @JsonProperty("shipping")
-        @SerializedName("shipping")
-        SHIPPING,
-        @JsonProperty("delivery")
-        @SerializedName("delivery")
-        DELIVERY,
-        @JsonProperty("availability")
-        @SerializedName("availability")
-        AVAILABILITY,
-        @JsonProperty("servicefulfilment")
-        @SerializedName("servicefulfilment")
-        SERVICEFULFILMENT,
-        @JsonProperty("other")
-        @SerializedName("other")
-        OTHER
-    }
-
-    public enum AmountPaymentType {
-        @JsonProperty("pay")
-        @SerializedName("pay")
-        PAY,
-        @JsonProperty("payupto")
-        @SerializedName("payupto")
-        PAYUPTO
-    }
 }
